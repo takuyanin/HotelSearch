b4e3b5d93871d43fb0bc129f2e66861eabda0e35
takuyanin <ftakuyamax0701@gmail.com>
2019-09-16 19:55:22 +0900

    Dispatch with ajax

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const mapStateToProps = state => ({
-  place: state.place,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onPlaceChange: place => dispatch({ type: 'CHANGE_PLACE', place }),
-});
+import { geocode } from '../domain/Geocoder'
+import { RAKUTEN_APP_ID } from '../../.env.js'
 
 const SearchForm = props => (
-  <form className='search-form' onSubmit={e => props.onSubmit(e)}>
+  <form
+    className='search-form'
+    onSubmit={(e) => {
+      e.preventDefault();
+      props.onSubmit(props.place)
+    }}
+  >
     <input
       className='place-input'
       size='30'
@@ -32,7 +33,63 @@ SearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 
-const ConnectedSearchForm =
-  connect(mapStateToProps, mapDispatchToProps)(SearchForm);
+const mapStateToProps = state => ({
+  place: state.place,
+});
 
-export default ConnectedSearchForm;
+const mapDispatchToProps = dispatch => ({
+  onPlaceChange: place => dispatch({ type: 'CHANGE_PLACE', place }),
+  onSubmit: (place) => {
+    geocode(place)
+      .then(({ status, address, location }) => {
+        switch (status) {
+          case 'OK': {
+            dispatch({ type: 'GEOCODE_FETCHED', address, location });
+            const params = {
+              applicationId: RAKUTEN_APP_ID,
+              datumType: 1,
+              latitude: location.lat,
+              longitude: location.lng,
+            };
+            axios.get(SIMPLE_HOTEL_SEARCH_ENDPOINT, { params })
+              .then((result) => {
+                const hotels = result.data.hotels.map((hotel) => {
+                  const basicInfo = hotel.hotel[0].hotelBasicInfo;
+                  const distance = getDistance(
+                    { latitude: location.lat, longitude: location.lng },
+                    { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+                  );
+                  return {
+                    id: basicInfo.hotelNo,
+                    name: basicInfo.hotelName,
+                    url: basicInfo.hotelInformationUrl,
+                    thumbUrl: basicInfo.hotelThumbnailUrl,
+                    price: basicInfo.hotelMinCharge,
+                    reviewAverage: basicInfo.reviewAverage,
+                    reviewCount: basicInfo.reviewCount,
+                    distance,
+                  };
+                })
+                this.setState({ hotels: sortedHotels(hotels, this.state.sortKey) });
+              });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            // this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            console.log(status)
+            // this.setErrorMessage('エラーが発生しました')
+          }
+        }
+        return [];
+      })
+      // .catch((error) => {
+      //   console.log(error)
+      //   this.setErrorMessage('通信に失敗しました')
+      // })
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
 ReactDOM.render(
   <Provider store={store}>
     <SearchPage
