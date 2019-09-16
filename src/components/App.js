e155d195720d149e76568cbf76aee5fcb79cf633
takuyanin <ftakuyamax0701@gmail.com>
2019-09-16 11:39:24 +0900

    Extract SearchPage and add left navi

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SearchPage from './SearchPage';
+
+const App = () => (
+  <div className='app'>
+    <ul className='left-navi'>
+      <li><a href='/'>ホテル検索</a></li>
+      <li><a href='/about'>About</a></li>
+    </ul>
+    <SearchPage />
+  </div>
+ )
+
+export default App;
